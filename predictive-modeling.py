0995634aae1ade240617fd726ac0ddcdfa0b3952
nguyenscg <nguyencsg@gmail.com>
2024-12-16 16:33:22 -0600

    initialize file upload

---
+++ predictive-modeling.py
@@ -0,0 +1,6 @@
+# Predictive Modeling For Game Outcomes
+# Purpose of this project is to predict whether a team wins or loses based on game and player stats
+# 1. Data Collect and generate dataset
+# 2. Data Process
+# 3. Model Development
+# 4. Visual and Presentation
